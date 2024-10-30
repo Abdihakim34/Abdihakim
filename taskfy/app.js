655c4a35617ce5eb5216bf4f2f496ff72c28f620
Abdihakim34 <hakiim202020@gmail.com>
2024-10-30 23:51:55 +0300

    Add files via upload

---
+++ taskfy/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const HOSTNAME = 'localhost'
+const PORT =  9000
+
+
+const server = http.createServer();
+
+server.listen(PORT,HOSTNAME,() => {
+
+    console.log('server running on port ${PORT}')
+})
