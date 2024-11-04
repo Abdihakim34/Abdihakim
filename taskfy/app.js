b133cec0efac946c7a55b30242a506443f65ad21
Abdihakiim <hakiim202020@gmail.com>
2024-11-04 22:17:19 +0300

    Add files via upload

--- taskfy/app.js
+++ taskfy/app.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const taskRoutes = require('./routes/taskRoutes');
+
 const HOSTNAME = 'localhost'
-const PORT =  9000
+const PORT = 9000
 
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith('/tasks')) {
+        taskRoutes(req, res)
+    } else {
+        res.writeHead(404, 'Not Found', { 'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: 'Sorry, you got lost!'
+        }))
+    }
+});
 
-const server = http.createServer();
-
-server.listen(PORT,HOSTNAME,() => {
-
-    console.log('server running on port ${PORT}')
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running on port ${PORT}`)
 })
