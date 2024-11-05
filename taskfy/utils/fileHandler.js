ae728bbf5b14a1ae06cbf58899b148acedc72278
Abdihakiim <hakiim202020@gmail.com>
2024-11-05 22:30:17 +0300

    Add files via upload

--- taskfy/utils/fileHandler.js
+++ taskfy/utils/fileHandler.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
 const filePath = './data/tasks.json';
-
 exports.writeTasksToFile = (tasks) => {
     if (Array.isArray(tasks) && tasks.length === 0) {
         fs.writeFileSync(filePath, '[]');
